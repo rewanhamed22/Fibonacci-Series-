f051350fdfc0e9931e8953bf9d485a47e9512200
Rewan Hamed <77508295+rewanhamed22@users.noreply.github.com>
2023-09-19 17:23:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fibonacciGenerator (n) {
  var output = [];
- if (n===1){ output=[0];}
- else if (n===2){ output=[0,1] ; }
+ if (n===1)
+ {
+  output=[0];
+ }
+ else if (n===2){ 
+  output=[0,1] ;
+ }
 else {
     output = [0,1];
     for (var i=2; i<n ;i++){
